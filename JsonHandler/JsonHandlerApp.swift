c5f377b0c5a561b009d340c90da3c215758c9939
sbmallik <smallik@gannett.com>
2024-01-25 11:26:27 -0500

    Initial Commit

---
+++ JsonHandler/JsonHandlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonHandlerApp.swift
+//  JsonHandler
+//
+//  Created by Mallik, Sudip on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct JsonHandlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
